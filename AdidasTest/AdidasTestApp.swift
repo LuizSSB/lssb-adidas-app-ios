b3dc2b35d4674c944381233f095ed9308549345a
Luiz S.S. Baglie <luizssb.biz@gmail.com>
2021-03-27 08:54:35 -0300

    Initial Commit

---
+++ AdidasTest/AdidasTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdidasTestApp.swift
+//  AdidasTest
+//
+//  Created by Luiz SSB on 27/03/21.
+//
+
+import SwiftUI
+
+@main
+struct AdidasTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdidasTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdidasTest
+//
+//  Created by Luiz SSB on 27/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
